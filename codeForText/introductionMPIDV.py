1da71624dbe1f41cb4e4a27acba4048da746e9b7
Tobias Panek <e12119891@student.tuwien.ac.at>
2024-07-19 02:14:56 +0200

    Add introduction code.

--- codeForText/introductionMPIDV.py
+++ codeForText/introductionMPIDV.py
@@ -13,4 +13,11 @@ single_trace = db.get_timing_data("name_of_trace")
 single_chunk = single_trace.create_chunk()
 chunk_list = db.create_chunks()
 
+# Listing 3.3
+single_chunk.filter_column("callid", lambda id: id == 1)
 
+# Listing 3.4
+chunk_list.each_filter_entities(
+    entity_selection_list=[0, 1, 2, 3, 4, 5, 6, 7],
+    entity_selection_lambda=lambda x: x % 8 == 0,
+    additional_selection=Chunk.Filter.MIN | Chunk.Filter.MAX)
